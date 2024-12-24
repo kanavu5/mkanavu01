2926211b522f5ef5ea381b90548516bbdf3c7001
kanavu5 <mail.kanavumeipadavendum@gmail.com>
2024-12-24 14:34:35 +0530

    Update app.py
    sample

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
 import streamlit as st
 
+def main():
+   
+    st.sidebar.image("banner.jpg", use_column_width=True)
+    # Add a sidebar for navigation
+    page = st.sidebar.selectbox("Select a page", ["Home", "Collage"])
+  
+
+    
+
+    if page == "Home":
+        home_page()
+    elif page == "Collage":
+        collage_page()
+
+if __name__ == "__main__":
+    main()
+  
 st.title("Hello, World!")
 st.write("This is a simple Streamlit app showing Hello, World!")
