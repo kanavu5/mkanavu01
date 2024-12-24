5d4849828efff8561073dd7e85354002d0f321ec
kanavu5 <mail.kanavumeipadavendum@gmail.com>
2024-12-24 20:59:11 +0530

    Update app.py
    test

--- app.py
+++ app.py
@@ -4,20 +4,14 @@ from home import home
 
 def main():
    
- 
     # Add a sidebar for navigation
     page = st.sidebar.selectbox("Select a page", ["Home", "Collage"])
   
-    
-
     if page == "Home":
         home()
     elif page == "Collage":
         home()
     
-
-   
-
 if __name__ == "__main__":
     main()
   
