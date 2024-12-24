79100e1e127fa14d8f7833055177c83ab4c74c60
kanavu5 <mail.kanavumeipadavendum@gmail.com>
2024-12-24 14:36:44 +0530

    Update app.py
    test

--- app.py
+++ app.py
@@ -2,17 +2,14 @@ import streamlit as st
 
 def main():
    
-    st.sidebar.image("banner.jpg", use_column_width=True)
+ 
     # Add a sidebar for navigation
     page = st.sidebar.selectbox("Select a page", ["Home", "Collage"])
   
 
     
 
-    if page == "Home":
-        home_page()
-    elif page == "Collage":
-        collage_page()
+   
 
 if __name__ == "__main__":
     main()
