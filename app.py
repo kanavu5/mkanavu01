a2b7ba48a3b02893dd3b16332f6afceebf12e963
kanavu5 <mail.kanavumeipadavendum@gmail.com>
2025-02-09 20:36:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 import streamlit as st
 from home import home
+from cutoff_page import cutoff
 
+# Set the page layout to wide
+st.set_page_config(layout="wide")
 
 def main():
-   
     # Add a sidebar for navigation
-    page = st.sidebar.selectbox("Select a page", ["Home", "Collage"])
+    page = st.sidebar.selectbox("Select a page", ["Home", "Collage", "Cutoff"])
   
     if page == "Home":
         home()
     elif page == "Collage":
         home()
+    elif page == "Cutoff":
+        cutoff()
     
 if __name__ == "__main__":
     main()
-  
-st.title("Hello, World!")
-st.write("This is a simple Streamlit app showing Hello, World!")
+
