493ffc23e0f8f645bc0f8a9119247784e3e0e266
kanavu5 <mail.kanavumeipadavendum@gmail.com>
2025-02-16 21:22:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def main():
     # Display the image and title side by side
     col1, col2 = st.columns([1, 5])
     with col1:
-        st.image("./logo.png", width=150)
+        st.image("./logo.PNG", width=150)
     with col2:
         st.title("Kanavu Meipada Vendum")
         st.markdown("""
