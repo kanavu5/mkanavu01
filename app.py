8702f0128b5fd028830d48218387c3091dd487cd
kanavu5 <mail.kanavumeipadavendum@gmail.com>
2025-02-16 21:14:07 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,42 @@
 import streamlit as st
 from home import home
 from cutoff_page import cutoff
+from collegelist import collagelist
 
 # Set the page layout to wide
-st.set_page_config(layout="wide")
+st.set_page_config(page_title='Snowflake', layout='wide')
 
 def main():
-    # Add a sidebar for navigation
-    page = st.sidebar.selectbox("Select a page", ["Home", "Collage", "Cutoff"])
+    # Display the image and title side by side
+    col1, col2 = st.columns([1, 5])
+    with col1:
+        st.image("./logo/logo.png", width=150)
+    with col2:
+        st.title("Kanavu Meipada Vendum")
+        st.markdown("""
+                Let your dreams come true! 🌨️   
+                 """)
+
+
+    _, exp_col, _ = st.columns([1,3,1])
+    with exp_col:
+        with st.expander("**📖  About This Website**"):
+            st.markdown("""
+                        This website is designed to assist students by providing valuable admission-related information for medical colleges, including details on previous years’ cutoffs. The information presented here is gathered from various reliable sources to serve as a guidance tool for aspiring students.
+
+                        While we strive to offer accurate and helpful insights, we strongly recommend that students verify the latest updates from official sources before making any decisions. Our goal is to simplify the admission process by making crucial data easily accessible, helping students make informed choices about their medical careers.
+
+                        Stay informed, plan wisely, and achieve your dreams
+                        """)
+        
+
+        # Add a sidebar for navigation
+        page = st.sidebar.selectbox("Select a page", ["Home", "Collage", "Cutoff"])
   
     if page == "Home":
         home()
     elif page == "Collage":
-        home()
+        collagelist()
     elif page == "Cutoff":
         cutoff()
     
---
+++ collegelist.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import pandas as pd
+import json
+import requests
+
+def collagelist():
+    # Add a banner image
+
+    
+    try:
+        # Load JSON data
+        url = st.secrets["data"]["data_url"]
+        response = requests.get(url)
+        data = response.json()
+  
+        # Convert JSON data to DataFrame
+        df = pd.DataFrame(data)
+        
+        # Capitalize column titles
+        df.columns = [col.upper() for col in df.columns]
+        
+        # Search and sort functionality in the same row
+        col1, col2 = st.columns(2)
+        with col1:
+            search_query = st.text_input("Search")
+        with col2:
+            sort_by = st.selectbox("Sort by", options=df.columns)
+            sort_order = st.radio("Sort order", options=["Ascending", "Descending"], horizontal=True)
+        
+        if search_query:
+            df = df[df['COUNTRY'].str.contains(search_query, case=False, na=False)]
+        
+        df = df.sort_values(by=sort_by, ascending=(sort_order == "Ascending"))
+        
+        # Pagination
+        records_per_page = 10
+        total_pages = (len(df) - 1) // records_per_page + 1
+        page = st.number_input('Page', min_value=1, max_value=total_pages, step=1)
+        
+        start_idx = (page - 1) * records_per_page
+        end_idx = start_idx + records_per_page
+        df_page = df.iloc[start_idx:end_idx]
+        
+        # Display DataFrame as a table without row numbers
+        st.table(df_page.reset_index(drop=True))
+    except FileNotFoundError:
+        st.error("The file 'data.json' was not found.")
+    except json.JSONDecodeError:
+        st.error("Error decoding JSON from the file 'data.json'.")
+    except ValueError:
+        st.error("Error converting JSON data to DataFrame.")
